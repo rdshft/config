91bdba0ea75dd15e2a2dc26fa8a30c09cecceb48
redshifttt <num@privatevoid.net>
2025-05-25 18:13:48 +0100

    nvim: deprecate lsp-zero; init vim.lsp.enable etc

--- home/sean/.config/nvim/lua/plugins/lsp.lua
+++ home/sean/.config/nvim/lua/plugins/lsp.lua
@@ -1,55 +1,65 @@
 return {
     "neovim/nvim-lspconfig",
-    dependencies = {
-        'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'
-    },
     config = function()
-        local lsp_zero = require('lsp-zero')
-        local lspconfig = require('lspconfig')
-
-        lsp_zero.set_preferences({ suggest_lsp_servers = false })
-
-        lsp_zero.on_attach(
-            ---@diagnostic disable-next-line: unused-local
-            function(client, bufnr)
-                lsp_zero.default_keymaps({ buffer = bufnr })
-            end
-        )
-
-        lspconfig.lua_ls.setup(
-            lsp_zero.nvim_lua_ls()
-        )
-
-        lsp_zero.setup_servers({
+        vim.lsp.enable({
+            'lua_ls',
             'ruff',
             'pylsp',
             'gopls',
         })
 
-        lsp_zero.set_sign_icons({
-            error = '',
-            warn = '',
-            hint = '',
-            info = ''
+        vim.lsp.config('lua_ls', {
+            on_init = function(client)
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    runtime = {
+                        version = 'LuaJIT',
+                        path = {
+                            'lua/?.lua',
+                            'lua/?/init.lua',
+                        },
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                        }
+                    }
+                })
+            end,
+            settings = {
+                Lua = {}
+            }
         })
 
-        lsp_zero.setup()
-
-        vim.keymap.set("n", "<F2>", function ()
-            local current_clients = #vim.lsp.get_clients({
-                bufnr = vim.api.nvim_get_current_buf()
-            })
-
-            if current_clients == 0 then
-                local current_word = vim.fn.expand("<cword>")
-                return ":%s/" .. current_word .. "/" .. current_word .. "/g"
-            else
-                vim.lsp.buf.rename()
-            end
-        end, {
-            -- need this for the string above to actually be interpreted.
-            -- see https://neovim.io/doc/user/lua-guide.html#_creating-mappings
-            expr = true
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = '',
+                    [vim.diagnostic.severity.WARN] = '',
+                    [vim.diagnostic.severity.INFO] = '',
+                    [vim.diagnostic.severity.HINT] = '',
+                },
+            },
         })
+
+        -- TODO: fix this, not sure why it doesn't work sometimes
+        -- vim.keymap.set("n", "<F2>", function ()
+        --     local current_clients = #vim.lsp.get_clients({
+        --         bufnr = vim.api.nvim_get_current_buf()
+        --     })
+        --
+        --     if current_clients == 0 then
+        --         local current_word = vim.fn.expand("<cword>")
+        --         return ":%s/" .. current_word .. "/" .. current_word .. "/g"
+        --     else
+        --         vim.lsp.buf.rename()
+        --     end
+        -- end, {
+        --     -- need this for the string above to actually be interpreted.
+        --     -- see https://neovim.io/doc/user/lua-guide.html#_creating-mappings
+        --     expr = true
+        -- })
     end
 }
