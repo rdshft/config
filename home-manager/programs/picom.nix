1eb476fced7e2da6ce4be730ceb0d412fd2d39df
redshifttt <num@privatevoid.net>
2026-09-11 20:06:56 +0100

    remove steam from compositing

--- home-manager/programs/picom.nix
+++ home-manager/programs/picom.nix
@@ -8,5 +8,17 @@
     activeOpacity = 1;
     shadow = false;
     vSync = true;
+
+    extraConfig = ''
+      rules = (
+        {
+          match = "class_g = 'steam' || class_g = 'Steam' || class_g = 'steamwebhelper'";
+          fade = false;
+          shadow = false;
+          full-shadow = false;
+          unredir = "forced";
+        }
+      )
+    '';
   };
 }
